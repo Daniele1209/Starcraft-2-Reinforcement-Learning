a5b93454804c119936b8d547c4a6f1f7e34dbbe5
Moș Daniele <dany.mosh@yahoo.com>
2022-10-14 01:40:40 +0300

    update: basic defense strategy for sc2 bot (pylons, turrets)

--- sc2-bot.py
+++ sc2-bot.py
@@ -1,3 +1,5 @@
+from re import L
+from unittest.util import unorderable_list_difference
 from sc2.bot_ai import BotAI  # parent class we inherit from
 from sc2.data import Difficulty, Race  # difficulty for bots, race for the 1 of 3 races
 from sc2.main import run_game  # function that facilitates actually running the agents in games
@@ -5,9 +7,46 @@ from sc2.player import Bot, Computer  #wrapper for whether or not the agent is o
 from sc2 import maps  # maps method for loading maps to play in.
 import sc2 
 
+from sc2.ids.unit_typeid import UnitTypeId
+import random
+
+
 class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
     async def on_step(self, iteration: int): # on_step is a method that is called every step of the game.
         print(f"This is my bot in iteration {iteration}") # prints out the iteration number (ie: the step).
+
+        if self.townhalls:
+            nexus = self.townhalls.random
+
+            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE):
+                nexus.train(UnitTypeId.PROBE)
+
+            elif not self.structures(UnitTypeId.PYLON) and self.already_pending(UnitTypeId.PYLON) == 0:
+                if self.can_afford(UnitTypeId.PYLON):
+                    await self.build(UnitTypeId.PYLON, near=nexus)
+
+            elif self.structures(UnitTypeId.PYLON).amount < 5:
+                if self.can_afford(UnitTypeId.PYLON):
+                    # target the closest pylon
+                    target_pylon = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0])
+                    # build towards the enemy start location
+                    pos = target_pylon.position.towards(self.enemy_start_locations[0], random.randrange(8, 15))
+                    await self.build(UnitTypeId.PYLON, near=pos)
+
+            # build cannons for defense -> we need a forge first
+            elif not self.structures(UnitTypeId.FORGE):
+                if self.can_afford(UnitTypeId.FORGE):
+                    await self.build(UnitTypeId.FORGE, near=self.structures(UnitTypeId.PYLON).closest_to(nexus))
+
+            elif self.structures(UnitTypeId.FORGE).ready and self.structures(UnitTypeId.PHOTONCANNON).amount < 3:
+                if self.can_afford(UnitTypeId.PHOTONCANNON):
+                    await(self.build(UnitTypeId.PHOTONCANNON, near=nexus))
+
+
+        else:
+            if self.can_afford(UnitTypeId.NEXUS):
+                await self.expand_now()
+
 
 run_game(  # run_game is a function that runs the game.
     maps.get("2000AtmospheresAIE"), # the map we are playing on
